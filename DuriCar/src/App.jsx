f28e541570bf1e05dd981fd8cef220faec1d7742
rjsdn031 <rjsdn031@naver.com>
2024-08-09 03:34:08 +0900

    [Feat] GoogleMap 연동
    google react wrapper, google map api 연동
    env에 키 protect
    추가 라이브러리 설치
    
    Fixes: Map main에 표시 안 됨

--- DuriCar/src/App.jsx
+++ DuriCar/src/App.jsx
@@ -1,33 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Status, Wrapper } from "@googlemaps/react-wrapper";
+import GoogleMap from "./components/GoogleMap"
+
+const render = (status) => {
+  switch (status) {
+    case Status.LOADING:
+      return <>로딩중...</>;
+    case Status.FAILURE:
+      return <>에러 발생</>;
+    case Status.SUCCESS:
+      return <GoogleMap />
+  }
+};
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Wrapper apiKey={import.meta.env.VITE_GOOGLEMAP_KEY} render={render} />
     </>
   )
 }
