c7631cce3a8896aee31c00f95bd246b81e9bab50
rjsdn031 <rjsdn031@naver.com>
2024-08-10 22:41:34 +0900

    [Test] 클릭 위치 확인 테스트 코드 사용
    확인용 내부 기능 코드 추가.
    지도 화면 클릭시 현재 위치 좌표 출력.

--- DuriCar/src/components/GoogleMap.jsx
+++ DuriCar/src/components/GoogleMap.jsx
@@ -8,6 +8,7 @@ function GoogleMap() {
 
     const ref = useRef(null);
     const [googleMap, setGoogleMap] = useState();
+    const [clickPosition, setClickPosition] = useState({lat:null, lng:null});
 
     useEffect(() => {
         if (ref.current) {
@@ -17,30 +18,35 @@ function GoogleMap() {
                     lng: 127.0,
                 },
                 zoom: 16,
+                mapId: '47775df8eb56b6df',
 
                 // Standard UI control
                 zoomControl: false,
                 mapTypeControl: false,
                 streetViewControl: false,
                 rotateControl: false,
-                fullscreenControl: false
+                fullscreenControl: false,
+
+                minZoom: 10,
+                maxZoom: 18,
             });
 
-            // initialMap.addListener("click", (event) => {
-            //     const lat = event.latLng.lat();
-            //     const lng = event.latLng.lng();
-            //     setClickPosition({ lat, lng }); // 클릭한 위치의 위도와 경도를 상태에 저장
-            //     console.log("Clicked location:", { lat, lng }); // 콘솔에 출력
-            // });
+            initialMap.addListener("click", (event) => {
+                const lat = event.latLng.lat();
+                const lng = event.latLng.lng();
+                setClickPosition({ lat, lng }); // 클릭한 위치의 위도와 경도를 상태에 저장
+                console.log("Clicked location:", { lat, lng }); // 콘솔에 출력
+            });
 
             setGoogleMap(initialMap);
         }
+
     }, []);
 
     return (
         <Container fluid="xs" className="d-flex justify-content-center align-items-center">
             <div id="map" ref={ref} />
-            {googleMap && (
+            {/* {googleMap && (
                 <Marker
                     map={googleMap}
                     position={{ 
@@ -49,7 +55,7 @@ function GoogleMap() {
                         }}
                     title="Hello World!"
                 />
-            )}
+            )} */}
         </Container>
     );
 }
